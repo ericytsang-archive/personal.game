e64d83c45fef2b4a72761dbf6f2fa38499240779
Eric Tsang <surplus.et@gmail.com>
2015-04-12 20:11:47 -0700

    initial commit
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ src/net/Client.java
@@ -0,0 +1,62 @@
+package net;
+
+import java.net.Socket;
+
+public interface Client
+{
+    /**
+     * initiates a new connection to the remote server at the address
+     *   {remoteAddr}:{remotePort}. when the socket connects, the onConnect
+     *   callback will be invoked. if the socket fails to connect, the
+     *   {onConnectFail} callback is invoked.
+     *
+     * @param    remoteAddr   IP address of the remote host.
+     * @param    remotePort   port number of the remote host to connect to.
+     *
+     * @return   returns the socket that is performing the connect call.
+     */
+    public abstract Socket connect(String remoteAddr, int remotePort);
+
+    /**
+     * sends a message to the client identified by the connection object.
+     *
+     * @param sock connection to send a message to
+     * @param packet packet to send from the socket.
+     */
+    public abstract void sendMessage(Socket sock, Packet packet);
+
+    /**
+     * callback invoked when a new connection is established with the server.
+     *
+     * @param conn socket that is created to communicate with the new
+     *   connection.
+     */
+    public abstract void onConnect(Socket conn);
+
+    /**
+     * callback invoked when a socket attempting to connect fails to connect.
+     *
+     * @param conn   socket that is created to communicate with the new
+     *   connection.
+     * @param e   exception that occurred on the socket.
+     */
+    public abstract void onConnectFail(Socket conn, Exception e);
+
+    /**
+     * callback invoked when a message from a connection is received.
+     *
+     * @param sock socket that the message was received from.
+     * @param packet packet received from the socket.
+     */
+    public abstract void onMessage(Socket sock, Packet packet);
+
+    /**
+     * callback invoked when the socket is closed by either the server, or the
+     *   client.
+     *
+     * @param sock socket that was closed.
+     * @param remote true if the socket was closed by the remote host; false
+     *   otherwise.
+     */
+    public abstract void onClose(Socket sock, boolean remote);
+}
---
+++ src/net/Packet.java
@@ -0,0 +1,196 @@
+package net;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * fancy wrapper for a stack so that data can be pushed onto the packet, and
+ *   popped off of it. it is useful when things want to push their own headers
+ *   onto the packet, so they can be popped off later.
+ *
+ * provides methods to serialize the {Packet} into a byte array, and deserialize
+ *   a byte array back into a {Packet}. these methods are used when the {Packet}
+ *   is being sent and received on the network.
+ *
+ * a {Packet} can be sent over the network using the {Client} and {Server}
+ *   subclasses.
+ *
+ * @author Eric Tsang
+ */
+public class Packet
+{
+    /**
+     * the packet's internal stack of data, where data is pushed onto, and
+     *   popped off.
+     */
+    private final Stack<byte[]> packetData;
+
+    /**
+     * length of the actual data encapsulated in this packet, not including
+     *   delimiters, or escape characters. just pure data.
+     */
+    private int packetLength;
+
+    //////////////////
+    // constructors //
+    //////////////////
+
+    /**
+     * instantiates a new packet instance.
+     */
+    public Packet()
+    {
+        packetData = new Stack<>();
+        packetLength = 0;
+    }
+
+    //////////////////////
+    // public interface //
+    //////////////////////
+
+    /**
+     * parses {data}, and replaces the data in the packet with the parsed data.
+     *
+     * @param    data   data to parse into the packet. this data should have
+     *   been created using the Packet::toBytes method.
+     *
+     * @return   returns a this pointer.
+     */
+    public Packet fromBytes(byte[] data)
+    {
+        return fromBytes(data,data.length);
+    }
+
+    /**
+     * parses {data}, and replaces the data in the packet with the parsed data.
+     *
+     * @param    data   data to parse into the packet. this data should have
+     *   been created using the Packet::toBytes method.
+     *
+     * @return   returns a this pointer.
+     */
+    public Packet fromBytes(byte[] data, int length)
+    {
+        // reset the state of this packet
+        packetData.clear();
+        packetLength = 0;
+
+        // go through the raw data, and put it into our reversed stack
+        ByteBuffer rawData = ByteBuffer.wrap(data,0,length);
+        Stack<byte[]> reversedSections = new Stack<>();
+        while(rawData.hasRemaining())
+        {
+            byte[] sectionData = new byte[rawData.getInt()];
+            rawData.get(sectionData,0,sectionData.length);
+            reversedSections.push(sectionData);
+        }
+
+        // move the data from the reversed stack onto the real one
+        while(reversedSections.size() > 0)
+        {
+            pushData(reversedSections.pop());
+        }
+
+        // return the packet
+        return this;
+    }
+
+    /**
+     * returns the byte version of the packet that can be used to transmit "over
+     *   the wire".
+     *
+     * each section of data is preceded by a 32-bit integer that specifies its
+     *   length.
+     *
+     * @return   the byte version of the packet that can be used to transmit
+     *   "over the wire".
+     */
+    public byte[] toBytes()
+    {
+        // allocate all the bytes we need to hold the packet data
+        ByteBuffer packet = ByteBuffer.allocate(
+            packetLength+packetData.size()*4);
+
+        // go through the stack, and write it to the byte array
+        for(int i = packetData.size()-1; i >= 0; --i)
+        {
+            byte[] data = packetData.get(i);
+            packet.putInt(data.length);
+            packet.put(data);
+        }
+
+        // return the packet
+        return packet.array();
+    }
+
+    /**
+     * adds {data} as the new header of the packet, and the previous data
+     *   becomes "payload".
+     *
+     * @param    data   the new data to add to the packet as header data.
+     *
+     * @return   returns a this pointer, so calls to this method can be chained.
+     */
+    public Packet pushData(byte[] data)
+    {
+        // update book keeping variables
+        packetLength += data.length;
+
+        // put the data on our stack
+        packetData.push(data);
+
+        // return this, so we can chain stuff
+        return this;
+    }
+
+    /**
+     * returns the data of the current header of the packet, and removes it from
+     *   the packet.
+     *
+     * @return   the data of the current header of the packet, and removes it
+     *   from the packet.
+     */
+    public byte[] popData()
+    {
+        // pop the the data from the stack & parse it
+        byte[] data = packetData.pop();
+
+        // update book keeping variables
+        packetLength -= data.length;
+
+        // return the data
+        return data;
+    }
+
+    /**
+     * returns the data of the current header of the packet, without removing it
+     *   from the packet.
+     *
+     * @return   the data of the current header of the packet, without removing
+     *   it from the packet.
+     */
+    public byte[] peekData()
+    {
+        // peek the the data from the stack, parse it & return it
+        return packetData.peek();
+    }
+
+    public static void main(String[] args)
+    {
+        Packet p = new Packet();
+        p.pushData(new byte[] {0,1,2,3,4,5});
+        p.pushData(new byte[] {1,1,2,3,4,5});
+        p.pushData(new byte[] {2,1,2,3,4,5});
+        p.pushData(new byte[] {3,1,2,3,4,5});
+
+        System.out.println(Arrays.toString(p.toBytes()));
+
+        System.out.println(Arrays.toString(p.fromBytes(p.toBytes()).toBytes()));
+
+        System.out.println(Arrays.toString(p.popData()));
+        System.out.println(Arrays.toString(p.popData()));
+        System.out.println(Arrays.toString(p.popData()));
+        System.out.println(Arrays.toString(p.popData()));
+    }
+}
